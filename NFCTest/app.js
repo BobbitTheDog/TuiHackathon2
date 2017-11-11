ccf09bdcd267eefe447aab704b0007212dc8e29e
FufanuOfFunk <sunnyallman@hotmail.co.uk>
2017-11-11 22:41:08 +0000

    Changes to ajax function

--- NFCTest/app.js
+++ NFCTest/app.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
 	console.log("Page loaded.");
 });
 
-function PostLocation(passengerName){
+function PostLocation(passengerName) {
+
 	// use $.ajax here to do a POST to http://localhost:56709/api/passengers/passengerName, with data set to Location.
 	// see https://api.jquery.com/jquery.post/
 	$.ajax({
 	  type: "POST",
-	  url: url,
-	  data: data,
+	  url: "http://localhost:56709/api/passengers/passengerName",
+	  data: location,
 	  success: successFunction,
 	  dataType: "text"
 	});
---
+++ Passenger Tracker/Passenger Tracker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passenger_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string testLocation = ConfigurationManager.AppSettings["location"];
+            //Console.WriteLine(testLocation);
+            //getRequest("http://www.youtube.com");
+            string token;
+            Console.ReadLine();
+
+        }
+
+
+        async static void postRequest(string url)
+        {
+            HttpContent content = new FormUrlEncodedContent(<KeyValuePair<string, string>>);
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.PostAsync("http://www.youtube.com", content))
+            using (HttpContent content = response.Content)
+            {
+
+                string myContent = await content.ReadAsStringAsync();
+                Console.WriteLine(myContent);
+            }
+        }
+    }
+}
